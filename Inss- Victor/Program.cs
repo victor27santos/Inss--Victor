07bd09b8578227a4412966a48caa4ee689785614
USER <USER@USER-PC>
2021-10-24 13:28:40 -0300

    Adicionar arquivos de projeto.

---
+++ Inss- Victor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Inss__Victor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 0;
+            double salarioAumento = 0;
+            double salarioFinal = 0;
+            double aumento = 0;
+            double desconto = 0;
+            Console.Write("Apresente o salário do empregado:");
+            salario = double.Parse(Console.ReadLine());
+
+            Console.Write("Salário inicial do empregado :" + salario + "\n");
+
+            aumento = salario * 0.15;
+            salarioAumento = salario + aumento;
+
+            Console.Write("Salário inicial do empregado com aumento: " + salarioAumento + "\n");
+
+            desconto = salarioAumento * 0.8;
+            salarioFinal = salarioAumento - desconto;
+
+            Console.Write("Salario do empregado com desconto é:" + salarioFinal + "\n");
+
+            Console.ReadKey();
+        }
+    }
+}
